8a4b017ded1f716ced0d276e2ade2934b38ad42d
Sumayya <sumayya.feroze@builtinsoft.com>
2023-07-10 14:16:35 +0500

    add navigation

---
+++ feature/auth/src/main/java/com/app/auth/home/HomeScreen.kt
@@ -0,0 +1,211 @@
+import android.annotation.SuppressLint
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.app.auth.R
+import com.app.auth.home.adjustments.TextFieldWithEndDrawable
+
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainScreenView(){
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomNavigation(navController = navController) }
+    ) {
+        NavigationGraph(navController = navController)
+    }
+}
+
+
+@Composable
+fun BottomNavigation(navController: NavController) {
+    val items = listOf(
+        BottomNavItem.Home,
+        BottomNavItem.MyNetwork,
+        BottomNavItem.AddPost,
+        BottomNavItem.Notification,
+        BottomNavItem.Jobs
+    )
+    BottomNavigation(
+        backgroundColor = Color.Green,
+        contentColor = Color.Black
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+        items.forEach { item ->
+            BottomNavigationItem(
+                icon = { Icon(painterResource(id = item.icon), contentDescription = item.title) },
+                label = { Text(text = item.title,
+                    fontSize = 9.sp) },
+                selectedContentColor = Color.Black,
+                unselectedContentColor = Color.Black.copy(0.4f),
+                alwaysShowLabel = true,
+                selected = currentRoute == item.screen_route,
+                onClick = {
+                    navController.navigate(item.screen_route) {
+
+                        navController.graph.startDestinationRoute?.let { screen_route ->
+                            popUpTo(screen_route) {
+                                saveState = true
+                            }
+                        }
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController, startDestination = BottomNavItem.Home.screen_route) {
+        composable(BottomNavItem.Home.screen_route) {
+            com.app.auth.login.HomeScreen()
+        }
+        composable(BottomNavItem.MyNetwork.screen_route) {
+            NetworkScreen()
+        }
+        composable(BottomNavItem.AddPost.screen_route) {
+            AddPostScreen()
+        }
+        composable(BottomNavItem.Notification.screen_route) {
+            NotificationScreen()
+        }
+        composable(BottomNavItem.Jobs.screen_route) {
+            JobScreen()
+        }
+    }
+}
+
+
+@Composable
+fun HomeScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Home Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Composable
+fun NetworkScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Cyan)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "My Network Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+}
+
+@Composable
+fun AddPostScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Red)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Add Post Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+}
+
+
+@Composable
+fun NotificationScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Green)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Notification Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+}
+
+
+@Composable
+fun JobScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black)
+            .wrapContentSize(Alignment.Center)
+    ) {
+        Text(
+            text = "Jobs Screen",
+            fontWeight = FontWeight.Bold,
+            color = Color.White,
+            modifier = Modifier.align(Alignment.CenterHorizontally),
+            textAlign = TextAlign.Center,
+            fontSize = 20.sp
+        )
+    }
+}
+
+sealed class BottomNavItem(var title:String, var icon:Int, var screen_route:String){
+
+    object Home : BottomNavItem("Menu", R.drawable.menu,"home")
+    object MyNetwork: BottomNavItem("My Network",R.drawable.back_icon,"my_network")
+    object AddPost: BottomNavItem("Post",R.drawable.whatsapp_icon,"add_post")
+    object Notification: BottomNavItem("Notification",R.drawable.whatsapp_icon,"notification")
+    object Jobs: BottomNavItem("Jobs",R.drawable.next_icon,"jobs")
+}
+@Preview(device = Devices.PIXEL_4)
+@Composable
+fun BottomNavigationBarPreview() {
+    MainScreenView()
+}
